f74aa6f70e4056a479f529c3734dfbe5cae78b46
Kiyoshi Nomo <tokyoincidents.g@gmail.com>
2024-08-29 20:21:57 +0900

    TileLayerWithHeader ref. https://github.com/PaulLeCam/react-leaflet/issues/852#issuecomment-782525462

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer } from 'react-leaflet'
 import './Map.css';
+import { TileLayerWithHeader } from '../lib/TileLayerWithHeader';
 
 export const Map = () => {
   // 緯度軽度
@@ -9,9 +10,10 @@ export const Map = () => {
   const zoom = 13;
   return (
     <MapContainer center={position} zoom={zoom}>
-      <TileLayer
+      <TileLayerWithHeader
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        headers={{ Authorization: "Bearer 1234567890" }}
       />
     </MapContainer>
   )
